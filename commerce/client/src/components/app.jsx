552e9d27e40eeca2ba40c0cf119132ce43dc9dc4
aziz <khardaniaziz@gmail.com>
2021-07-26 22:46:54 +0100

    navbar

--- commerce/client/src/components/app.jsx
+++ commerce/client/src/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Navbare from './Navbar.jsx'
 
 export default class App extends React.Component{
 
@@ -7,7 +7,9 @@ export default class App extends React.Component{
 
     render(){
         return (
-            <div>ga3daa</div>
+            <div>
+                <Navbare />
+            </div>
         )
     }
 }
